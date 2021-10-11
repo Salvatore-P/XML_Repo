ef45594be273743df7a2c169abb62954cc0d6419
Salva <portosalvatore2@gmail.com>
2021-10-11 10:16:11 +0200

    debut

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,39 @@
 package com.company;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
-    public static void printTag(BufferedWriter bw, String tag, String data, int NbTab,boolean open, boolean close) throws IOException {
 
-        if(open)
-            bw.write("\t".repeat(NbTab) + "<" + tag + ">\n");
-        bw.write("\t".repeat(NbTab + 1) + data + "\n");
-        if(close)
-            bw.write("\t".repeat(NbTab) + "</" + tag  + ">\n");
+        try {
+            BufferedReader br = OpenBuffRe();
+            BufferedWriter bw = OpenBuffWr();
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void printTag(BufferedWriter bw, String tag, String data, int NbTab,boolean simple, boolean open, boolean close) throws IOException {
+        if(simple)
+            bw.write("\t".repeat(NbTab) + "<" + tag + "> " + data + " </" + tag + ">\n");
+        else{
+            if (open)
+                bw.write("\t".repeat(NbTab) + "<" + tag + ">\n");
+            bw.write("\t".repeat(NbTab + 1) + data + "\n");
+            if (close)
+                bw.write("\t".repeat(NbTab) + "</" + tag + ">\n");
+        }
+    }
+    public static BufferedWriter OpenBuffWr() throws IOException {
+
+        File file = new File("XML_Modifier.txt");
+        return new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+    }
+    public static BufferedReader OpenBuffRe() throws IOException {
+
+        return new BufferedReader(new FileReader(".\\.\\.\\Fichier\\1000movies.txt"));
     }
 }
